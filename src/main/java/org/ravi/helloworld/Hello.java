53c08420176b70a7374ff17660f7fe1760810e92
Training_M5.06.17 <pawarsupriya2412@gmail.com>
2018-02-21 15:13:47 +0530

    hgyjf

--- src/main/java/org/ravi/helloworld/Hello.java
+++ src/main/java/org/ravi/helloworld/Hello.java
@@ -3,8 +3,9 @@ package org.ravi.helloworld;
 public class Hello {
 	
 	public String displayHelloMessage(String message){
-		message = "hi";
+		message = "hi supriya hello";
 		return message;
 	}
 
 }
+
